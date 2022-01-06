051c5f8b5410882cced58ac619fbe7b40cf4c9b7
akdrhtn1 <akdrhtn@kakao.com>
2022-01-07 01:37:04 +0900

    update

--- backend/src/main/java/com/todotogether/config/SecurityConfig.java
+++ backend/src/main/java/com/todotogether/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
